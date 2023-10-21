df0fd68c1e4bef239bae3b90db741910d7edc11a
JasonCrk <emerzonkolki@gmail.com>
2023-10-21 16:14:07 -0500

    feat(MemorandumResponse.java): add assigned field to response

--- src/main/java/com/ADS2/controlinternoalmacenapi/response/memorandum/MemorandumResponse.java
+++ src/main/java/com/ADS2/controlinternoalmacenapi/response/memorandum/MemorandumResponse.java
@@ -1,5 +1,7 @@
 package com.ADS2.controlinternoalmacenapi.response.memorandum;
 
+import com.ADS2.controlinternoalmacenapi.response.usuario.UsuarioResponse;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +13,5 @@ public class MemorandumResponse {
     private Long id;
     private String title;
     private String createdAt;
+    private UsuarioResponse assigned;
 }
